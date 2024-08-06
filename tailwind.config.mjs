8fad746fc7fedb96cb8c7f9c66ffdd9c889f6ec3
Nefta Hernandez <neftaligow01@gmail.com>
2024-08-06 03:46:18 -0600

    feat: 🎸 Landing page
    The landing page is almost done, just need to fix some bugs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        "slide-logos": "slide-logos 60s linear infinite",
+      },
+      keyframes: {
+        "slide-logos": {
+          to: { transform: "translateX(calc(-50% - 2rem))" },
+        },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar-hide"),
+  ],
+};
